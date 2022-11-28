c02a10eeeaea31ce063477666d2a305b9c638f41
anshchawla521 <anshchawla521@gmail.com>
2022-11-28 21:39:42 +0530

    q1 complete

---
+++ q1.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+import javax.smartcardio.ATR;
+
+public class q1 {
+    public static void main(String args[])
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("enter first number");
+        int a = obj.nextInt();
+        System.out.println("enter second number");
+        int b = obj.nextInt();
+        System.out.println("enter 1 for multiplication and 2 for division");
+        int c = obj.nextInt();
+        try{
+        if (c == 1)
+        {
+            number.multiply(a, b);
+        }else if (c ==2)
+        {
+            number.Divison(a, b);
+        }
+        }catch (Exception ArithmeticException)
+        {
+            System.out.println("exception handeled");
+        }
+
+    }
+}
+
+public class number
+{
+    static float Divison(int a , int b)
+    {
+        if( a == 0 && b == 0)
+        {
+            throw new ArithmeticException("0/0 ");
+        }
+        return (float)a/b;
+    }
+
+    static float multiply(int a , int b)
+    {
+        if( a == 0 && b == 0)
+        {
+            throw new ArithmeticException("0*0");
+        }
+        return (float)a*b;
+    }
+}
